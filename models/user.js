651ea855d9311b4b19b0bf1d130a47b4b0db70cf
ChandrakantGawde <chandrakantGawde@gmail.com>
2024-12-15 00:19:50 +0530

    Authentication

--- models/user.js
+++ models/user.js
@@ -1,5 +1,7 @@
 const {createHmac, randomBytes} = require("node:crypto"); // Place this at the top of the file, outside of any function
 const {Schema, model} = require("mongoose");
+const { createTokenForUser } = require("../services/authentication");
+const { use } = require("../routes/user");
 
 const userSchema = new Schema({
     fullName: {
@@ -43,6 +45,21 @@ userSchema.pre("save", function (next) {
 
     next();
 });
+
+userSchema.static('matchPasswordAndGenrateToken',  async function(email, password){
+    const user = await this.findOne({email});
+    if(!user) throw new Error("User not found!");
+
+    const salt = user.salt;
+    const hashedPassword = user.password;
+
+    const userProvidedHash = createHmac("sha256", salt).update(password).digest("hex");
+    
+    if(hashedPassword !== userProvidedHash) throw new Error("Incorrect password");
+    
+    const token = createTokenForUser(user);
+    return token;
+})   
 
 const User = model("user", userSchema);
 module.exports = User;
--- routes/user.js
+++ routes/user.js
@@ -3,13 +3,26 @@ const User = require("../models/user");
 
 const router = Router();
 
-// not use controller er write all code in this file
+// not use controller write all code in this file
 router.get('/signin', (req, res)=> {
     return res.render("signin");
 });
 
 router.get('/signup', (req, res)=> {
     return res.render("signup");
+});
+
+router.post("/signin", async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const token = await User.matchPasswordAndGenrateToken(email, password);
+        return res.cookie('token', token).redirect("/");
+    } catch (error) {
+        return res.render("signin", {
+            error: "Incorrect Email or Password",
+        })
+    }
 });
 
 router.post("/signup", async (req, res)=> {
