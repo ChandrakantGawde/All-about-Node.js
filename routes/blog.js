59f6cc3766d89c343e3363379a9905ae771608ed
Chandrakant Gawde <chandrakantgawde3@gmail.com>
2024-12-28 20:00:58 +0530

    commentsAdd

--- routes/blog.js
+++ routes/blog.js
@@ -1,5 +1,6 @@
 const {Router} = require("express");
 const Blog = require("../models/blog");
+const Comment = require("../models/comment");
 const multer = require("multer");
 const path = require("path");
 
@@ -25,12 +26,24 @@ router.get("/add-new",  (req, res)=> {
 });
 
 router.get("/:id", async (req, res)=> {
-    const blog = await Blog.findById(req.params.id);
+    const blog = await Blog.findById(req.params.id).populate("createdBy");
+    const comments = await Comment.find({ blogId: req.params.id}).populate("createdBy");
+    console.log(comments,'comment');
     return res.render("blog", {
         user: req.user,
-        blog
+        blog,
+        comments,
     });
 });
+
+router.post('/comment/:blogId', async (req, res) => {
+       await Comment.create({
+       content: req.body.content,
+       blogId: req.params.blogId,
+       createdBy: req.user._id
+    });
+    return res.redirect(`/blog/${req.params.blogId}`)
+})
 
 router.post("/", upload.single('coverImage'), async (req, res)=> {
     console.log(req.body);
